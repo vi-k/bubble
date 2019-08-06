4336e0f29b9bf1e432ecf80d62fab002bfbd1282
vi-k <victor.dunaev@gmail.com>
2019-08-06 21:56:07 +1000

    For private testing

--- example/lib/test.dart
+++ example/lib/test.dart
@@ -1,15 +1,15 @@
-        child: ListView(
-          children: [
-            Divider(height: 40, color: Colors.black),
-            Bubble(
-              nip: BubbleNip.TOP_RIGHT,
-              child: Text('Hello, world!'),
-            ),
-            Bubble(
-              margin: BubbleEdges.only(top: 10),
-              nip: BubbleNip.TOP_LEFT,
-              child: Text('Hello, programmer!'),
-            ),
-            Divider(height: 40, color: Colors.black),
-          ],
-        ),
+child: ListView(
+  children: [
+    Divider(height: 40, color: Colors.black),
+    Bubble(
+      nip: BubbleNip.topRight,
+      child: Text('Hello, world!'),
+    ),
+    Bubble(
+      margin: BubbleEdges.only(top: 10),
+      nip: BubbleNip.topLeft,
+      child: Text('Hello, programmer!'),
+    ),
+    Divider(height: 40, color: Colors.black),
+  ],
+),
